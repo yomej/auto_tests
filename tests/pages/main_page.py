0f38f8923cb8e3eca9e0bbe1a1a6cf31083fdbb1
merg <tmerg74@gmail.com>
2023-01-10 07:35:45 +0700

    login_page.py надо добавить readme

--- tests/pages/main_page.py
+++ tests/pages/main_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
