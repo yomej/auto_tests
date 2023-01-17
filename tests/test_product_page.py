f5095bb7da90d7174e5ec2a51ac78dfb8b580ff6
merg <tmerg74@gmail.com>
2023-01-17 07:20:52 +0700

    add parametrize

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -11,10 +11,10 @@ urls[7] = pytest.param(urls[7], marks=pytest.mark.xfail)
 
 @pytest.mark.parametrize('link', urls)
 def test_guest_can_add_product_to_basket(browser, link):
-    product_page = ProductPage(browser, link)  # Создаём экземпляр страницы Product Page
-    product_page.open()  # Открываем в браузере
-    product_page.should_be_product_page()  # Проверка ссылки и наличия кнопки добавления в корзину, клик по кнопке
-    product_page.solve_quiz_and_get_code()  # Проведение вычислений с алерта и вывод результата
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_product_page()
+    product_page.solve_quiz_and_get_code()
     time.sleep(5)
     product_page.product_name()
     product_page.product_price()
